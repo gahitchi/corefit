93968951c936e67e56a9a1b053b571d7fef9db63
gahitchi <avemodz@gmail.com>
2025-03-21 08:33:03 +0100

    wearOS software
    wip

---
+++ corefit/app/src/main/java/com/project/corefit/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.project.corefit.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun CorefitTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
